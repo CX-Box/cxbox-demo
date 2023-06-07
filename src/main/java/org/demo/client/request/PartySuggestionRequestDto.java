f5dc6569ba813bfd31c0009b91a33670a8efb3e4
ryalalov <ryalalov@phoenixit.ru>
2023-06-07 17:55:53 +0300

    Added dadata integration example

---
+++ src/main/java/org/demo/client/request/PartySuggestionRequestDto.java
@@ -0,0 +1,32 @@
+package org.demo.client.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class PartySuggestionRequestDto {
+
+	private String query;
+
+	private Integer count;
+
+	private String type;
+
+	@JsonProperty("status")
+	private List<String> allowedStatuses;
+
+	@JsonProperty("okved")
+	private List<String> okvedFilter;
+
+	@JsonProperty("locations")
+	private List<Object> locationFilter;
+
+	@JsonProperty("locations_boost")
+	private List<Object> locationsBoost;
+
+}

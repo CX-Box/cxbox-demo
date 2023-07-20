8b97026c61cf1ab36e343b5eba46334a402f91cc
DROMANE <dmitry.romanenko@weigandt-consulting.com>
2023-07-21 00:11:23 +0300

    CXBOX-59 | fixed search display for filtering (MultipleSelect)

--- ui/src/components/ColumnTitle/FilterField.tsx
+++ ui/src/components/ColumnTitle/FilterField.tsx
@@ -4,26 +4,30 @@ import { ColumnFilterControlProps } from '@cxbox-ui/core/components/ui/FilterFie
 import { FieldType } from '@cxbox-ui/core/interfaces/view'
 import { DataValue } from '@cxbox-ui/core/interfaces/data'
 import { CheckboxFilter } from './CheckboxFilter/CheckboxFilter'
+import { CustomFieldTypes } from '../../interfaces/widget'
+import { NumberFieldMeta } from '@cxbox-ui/schema/dist/interfaces/widget'
 
 interface FilterFieldProps extends ColumnFilterControlProps {}
 
 function FilterField(props: FilterFieldProps) {
     const { widgetFieldMeta, value, onChange, rowFieldMeta } = props
 
-    switch (widgetFieldMeta.type) {
+    switch (widgetFieldMeta.type as string) {
         case FieldType.number:
         case FieldType.money:
         case FieldType.percent:
+            const fieldMeta = widgetFieldMeta as NumberFieldMeta
             return (
                 <NumberInput
                     value={value as number}
                     type={widgetFieldMeta.type as any}
                     onChange={onChange}
-                    digits={widgetFieldMeta.digits}
-                    nullable={widgetFieldMeta.nullable}
+                    digits={fieldMeta.digits}
+                    nullable={fieldMeta.nullable}
                     forceFocus={true}
                 />
             )
+        case CustomFieldTypes.MultipleSelect:
         case FieldType.radio: {
             return (
                 <CheckboxFilter

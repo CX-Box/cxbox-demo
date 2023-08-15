780b3fb4958830a6562848d7a512fe87ca4fcebb
igornb <igornb.mail@gmail.com>
2023-08-15 23:32:51 +0300

    fix local front development speed

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api/v1',
         createProxyMiddleware({
+            host: '[::1]',
             target: 'http://localhost:8080'
         })
     )

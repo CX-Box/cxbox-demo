aad4d5459de268df88af6210130b169c5a62a5d4
igornb <igornb.mail@gmail.com>
2023-08-18 16:25:27 +0300

    feature/ms-support

--- ui/src/reducers/data.ts
+++ ui/src/reducers/data.ts
@@ -10,6 +10,31 @@ const emptyData: DataItem[] = []
 
 export default function dataReducer(state: DataState = initialState, action: AnyAction, store?: Readonly<AppState>): DataState {
     switch (action.type) {
+        case actionTypes.sendOperationSuccess: {
+            const nextDataItem = action.payload.dataItem
+            if (nextDataItem != null) {
+                let spreadElements = {
+                    ...state,
+                    [action.payload.bcName]: (state[action.payload.bcName] || emptyData).map(item => {
+                        if (item.id === nextDataItem.id) {
+                            return nextDataItem
+                        } else if (item.id === action.payload.cursor && nextDataItem.id) {
+                            /**
+                             * Here we support id change on save action to support platform usage as other microservices data provider. I
+                             * In this case new record is usually virtually created with temporary id, then on 'save' record is saved to real microservice and temporary id is replaced with new permanent one
+                             */
+                            return nextDataItem
+                        } else {
+                            return item
+                        }
+                    })
+                }
+                return {
+                    ...spreadElements
+                }
+            }
+            return state
+        }
         case actionTypes.bcSaveDataSuccess: {
             const nextDataItem = action.payload.dataItem
             return {

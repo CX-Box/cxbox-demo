0cb71783bb2488a232dfe21e81d92e8e6de6abd3
Il'ya <33763627+Lourence94@users.noreply.github.com>
2024-01-26 15:00:58 +0300

    CXBOX-205 Added new AdditionalInfo widget (#103)
    CXBOX-205 Added new AdditionalInfo widget

--- ui/src/components/ui/DashboardLayout/DashboardLayout.tsx
+++ ui/src/components/ui/DashboardLayout/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Row, Col } from 'antd'
 import { AppWidgetMeta, CustomWidgetTypes } from '@interfaces/widget'
 import { createSkipWidgetList } from '@utils/createSkipWidgetList'
@@ -18,25 +18,50 @@ export function DashboardLayout(props: DashboardLayoutProps) {
     const widgetsByRow = React.useMemo(() => {
         return groupByRow(props.widgets, props.skipWidgetTypes || [])
     }, [props.widgets, props.skipWidgetTypes])
-    return (
-        <React.Fragment>
-            {Object.values(widgetsByRow).map((row, rowIndex) => (
-                <Row key={rowIndex}>
-                    {row.map((widget, colIndex) => (
-                        <Col key={colIndex} span={getColWidth(widget)}>
-                            <Widget
-                                meta={widget}
-                                card={props.card}
-                                customWidgets={props.customWidgets}
-                                customSpinner={props.customSpinner}
-                                disableDebugMode={props.disableDebugMode}
-                            />
-                        </Col>
-                    ))}
-                </Row>
+
+    const additionalInfoWidgets = useMemo(() => {
+        return props.widgets.filter(widget => widget.type === CustomWidgetTypes.AdditionalInfo)
+    }, [props.widgets])
+
+    const CommonWidgets = Object.values(widgetsByRow).map((row, rowIndex) => (
+        <Row key={rowIndex}>
+            {row.map((widget, colIndex) => (
+                <Col key={colIndex} span={getColWidth(widget)}>
+                    <Widget
+                        meta={widget}
+                        card={props.card}
+                        customWidgets={props.customWidgets}
+                        customSpinner={props.customSpinner}
+                        disableDebugMode={props.disableDebugMode}
+                    />
+                </Col>
             ))}
-        </React.Fragment>
-    )
+        </Row>
+    ))
+
+    if (additionalInfoWidgets.length !== 0) {
+        return (
+            <Row gutter={8}>
+                <Col span={18}>{CommonWidgets}</Col>
+                <Col span={6}>
+                    {additionalInfoWidgets.map(widget => (
+                        <Row key={widget.name}>
+                            <Col span={24}>
+                                <Widget
+                                    meta={widget}
+                                    customWidgets={props.customWidgets}
+                                    customSpinner={props.customSpinner}
+                                    disableDebugMode={props.disableDebugMode}
+                                />
+                            </Col>
+                        </Row>
+                    ))}
+                </Col>
+            </Row>
+        )
+    }
+
+    return <React.Fragment>{CommonWidgets}</React.Fragment>
 }
 
 function groupByRow<WidgetMeta extends AppWidgetMeta>(widgets: WidgetMeta[], skipWidgetTypes: string[]) {

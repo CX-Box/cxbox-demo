c5956f2ef786c03640586f69d39320afb949f771
Ilya Korneev <imkor34@mail.ru>
2024-01-26 16:25:32 +0300

    gap between widgets, enable debug menu for AdditionalInfo widget

--- ui/src/components/ui/DashboardLayout/DashboardLayout.tsx
+++ ui/src/components/ui/DashboardLayout/DashboardLayout.tsx
@@ -41,11 +41,11 @@ export function DashboardLayout(props: DashboardLayoutProps) {
 
     if (additionalInfoWidgets.length !== 0) {
         return (
-            <Row gutter={8}>
+            <Row gutter={24}>
                 <Col span={18}>{CommonWidgets}</Col>
                 <Col span={6}>
                     {additionalInfoWidgets.map(widget => (
-                        <Row key={widget.name}>
+                        <Row key={widget.name} gutter={[8, 8]}>
                             <Col span={24}>
                                 <Widget
                                     meta={widget}

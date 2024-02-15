ef4c297126979e2192e89064e83e2240a969d086
ikorneev <imkor34@mail.ru>
2024-02-15 20:51:11 +0300

    visual widget draft

---
+++ ui/src/components/widgets/StatsBlock/StatsBlock.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const StatsBlock: React.FC = () => {
+    return (
+        <div>
+            <span>block 1</span>
+            <span>block 2</span>
+            <span>block 3</span>
+            <span>block 4</span>
+            <span>block 5</span>
+        </div>
+    )
+}

7d63ca9cc262af4491236e18b57bce7b5d246986
romadim <100356095+romadim@users.noreply.github.com>
2024-03-22 19:06:14 +0300

    CXBOX-255 | changed display of total quantity (#153)
    Co-authored-by: DROMANE <dmitry.romanenko@weigandt-consulting.com>

--- ui/src/components/ui/Pagination/components/Limit.tsx
+++ ui/src/components/ui/Pagination/components/Limit.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Select } from 'antd'
 import { useTranslation } from 'react-i18next'
+import styles from './Limit.less'
 
 const DEFAULT_PAGE_LIMIT = 5
 const AVAILABLE_LIMITS_LIST = [5, 10, 15, 20]
@@ -17,13 +18,16 @@ function Limit({ className, onChange, value = DEFAULT_PAGE_LIMIT, options = AVAI
     const { t } = useTranslation()
 
     return (
-        <Select value={value} className={className} onSelect={onChange}>
-            {options.map(limit => (
-                <Select.Option key={limit} value={limit}>
-                    {t('limit of', { limit, total })}
-                </Select.Option>
-            ))}
-        </Select>
+        <div className={styles.root}>
+            <Select value={value} className={className} onSelect={onChange}>
+                {options.map(limit => (
+                    <Select.Option key={limit} value={limit}>
+                        {t('limit / page', { limit })}
+                    </Select.Option>
+                ))}
+            </Select>
+            {t('of total', { total })}
+        </div>
     )
 }
 

35503e36d1235fbc79f5d78aab3c635f383246ec
DROMANE <dmitry.romanenko@weigandt-consulting.com>
2024-04-17 17:37:26 +0300

    fixed

--- ui/src/interfaces/widget.ts
+++ ui/src/interfaces/widget.ts
@@ -128,11 +128,28 @@ export interface SuggestionPickListField extends Omit<interfaces.PickListFieldMe
 
 export type FileUploadFieldMeta = CoreFileUploadFieldMeta & {
     preview?: {
+        /**
+         * Enables file previews. Default false.
+         */
         enabled: boolean
+        /**
+         * Key whose value is used for the popup title. If not specified, the file name is taken
+         */
         titleKey?: string
+        /**
+         * The key whose value is used for the tooltip under the popup title. If not specified, the additional attribute is not shown.
+         */
         hintKey?: string
-        previewMode?: 'popup' | 'side-panel'
-        miniPreviewUrlKey?: string
+        /**
+         * Preview display mode: popup (default), side-panel.
+         */
+        mode?: 'popup' | 'side-panel'
+        /**
+         * Includes display of mini-previews for file types for which we can, for the rest there are icons with an eye.
+         * The default is false (icons with an eye are shown for all files).
+         * Where the value will come from is decided at the project level.
+         */
+        miniPreview?: boolean
     }
 }
 

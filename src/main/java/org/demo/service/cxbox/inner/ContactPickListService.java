510f4a0d05a5192d64922f35df65c5106f82c0d1
cxbox <cxbox.mail@gmail.com>
2024-07-25 02:50:30 +0300

    picklist inline create/edit added

--- src/main/java/org/demo/service/cxbox/inner/ContactPickListService.java
+++ src/main/java/org/demo/service/cxbox/inner/ContactPickListService.java
@@ -4,11 +4,16 @@ import org.cxbox.core.crudma.bc.BusinessComponent;
 import org.cxbox.core.crudma.impl.VersionAwareResponseService;
 import org.cxbox.core.dto.rowmeta.ActionResultDTO;
 import org.cxbox.core.dto.rowmeta.CreateResult;
+import org.cxbox.core.service.action.Actions;
 import org.cxbox.model.core.entity.BaseEntity_;
 import org.demo.dto.cxbox.inner.ContactDTO;
+import org.demo.dto.cxbox.inner.ContactDTO_;
+import org.demo.entity.Client;
 import org.demo.entity.Contact;
 import org.demo.entity.Contact_;
 import org.demo.entity.Meeting;
+import org.demo.repository.ClientRepository;
+import org.demo.repository.ContactRepository;
 import org.demo.repository.MeetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
@@ -20,6 +25,12 @@ public class ContactPickListService extends VersionAwareResponseService<ContactD
 
 	@Autowired
 	private MeetingRepository meetingRepository;
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private ContactRepository contactRepository;
 
 	public ContactPickListService() {
 		super(ContactDTO.class, Contact.class, null, ContactPickListMeta.class);
@@ -39,14 +50,29 @@ public class ContactPickListService extends VersionAwareResponseService<ContactD
 
 	@Override
 	protected CreateResult<ContactDTO> doCreateEntity(Contact entity, BusinessComponent bc) {
-		return null;
+		Client client = clientRepository.findById(bc.getParentIdAsLong()).orElse(null);
+		entity.setClient(client);
+		contactRepository.save(entity);
+		return new CreateResult<>(entityToDto(bc, entity));
 	}
 
 	@Override
 	protected ActionResultDTO<ContactDTO> doUpdateEntity(Contact entity, ContactDTO data,
 			BusinessComponent bc) {
-		return null;
+		setIfChanged(data, ContactDTO_.fullName, entity::setFullName);
+		setIfChanged(data, ContactDTO_.email, entity::setEmail);
+		setIfChanged(data, ContactDTO_.phoneNumber, entity::setPhoneNumber);
+		contactRepository.save(entity);
+		return new ActionResultDTO<>(entityToDto(bc, entity));
 	}
 
+	@Override
+	public Actions<ContactDTO> getActions() {
+		return Actions.<ContactDTO>builder()
+				.create().text("Add").add()
+				.save().text("Save").add()
+				.cancelCreate().text("Cancel").available(bc -> true).add()
+				.build();
+	}
 
 }

7a750b7633121cc56cd39b59701ebccef7750cca
cxbox <cxbox.mail@gmail.com>
2024-08-14 03:27:00 +0300

    moved to default spring-boot dependencies
    jobrunr-spring-boot-3-starter.version> moved to 7.1.1 to be in sync with spring-boot 3.2.5 (e.g. to be in sync with cxbox-starter-parent)
    
    removed mapstruct dependency
    removed springdoc-openapi-starter-webmvc-ui dependency
    removed datasource-proxy-spring-boot-starter dependency

--- src/main/java/org/demo/microservice/core/querylang/springdoc/core/QueryLanguageFilterParamsOperationCustomizer.java
+++
@@ -1,7 +0,0 @@
-package org.demo.microservice.core.querylang.springdoc.core;
-
-import org.springdoc.core.customizers.GlobalOperationCustomizer;
-
-public interface QueryLanguageFilterParamsOperationCustomizer extends GlobalOperationCustomizer {
-
-}

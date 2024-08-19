5f259708a704b3fcbdd22a682910119fbfda1ffd
DROMANE <dmitry.romanenko@weigandt-consulting.com>
2024-08-19 18:07:55 +0300

    CXBOX-454 | changed getting context for tabs

--- ui/src/components/ViewNavigation/tab/standard/hooks/useContextStandardViewTabs.ts
+++ ui/src/components/ViewNavigation/tab/standard/hooks/useContextStandardViewTabs.ts
@@ -1,49 +1,26 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback } from 'react'
 import { createUrlWithContext } from '@utils/createUrlWithContext'
 import { useStandardViewTabs } from '@components/ViewNavigation/tab/standard/hooks/useStandardViewTabs'
-import { useChangeLocation, getRouteFromString } from '@router'
+import { useChangeLocation } from '@router'
 import { useAppSelector } from '@store'
-import { usePrevious } from '@hooks/usePrevious'
-
-const getBcNameFromPath = (path?: string) => {
-    const bcPath = path ? getRouteFromString(path)?.bcPath : undefined
-
-    if (!bcPath) {
-        return ''
-    }
-
-    const enpPosition = bcPath.indexOf('/')
-
-    return bcPath.slice(0, enpPosition)
-}
 
 export const useContextStandardViewTabs = (depth = 1) => {
+    const bcPath = useAppSelector(state => state.router.bcPath ?? '')
     const needBcPathContext = depth !== 0
     const changeLocation = useChangeLocation()
-
-    const [contextBcName, setContextBcName] = useState<string>('')
-    const cachedBc = useAppSelector(state => state.screen.cachedBc)
-    const path = useAppSelector(state => state.router.path)
-    const prevPath = usePrevious(path)
-
-    useEffect(() => {
-        if (needBcPathContext && prevPath !== path) {
-            setContextBcName(getBcNameFromPath(path))
-        }
-    }, [prevPath, setContextBcName, path, needBcPathContext])
-
-    const pushWithContext = useCallback(
-        (urlWithoutBcPath: string) => {
-            changeLocation(createUrlWithContext(urlWithoutBcPath, cachedBc[contextBcName]))
-        },
-        [cachedBc, changeLocation, contextBcName]
-    )
 
     const push = useCallback(
         (url: string) => {
             changeLocation(url)
         },
         [changeLocation]
+    )
+
+    const pushWithContext = useCallback(
+        (urlWithoutBcPath: string) => {
+            changeLocation(createUrlWithContext(urlWithoutBcPath, bcPath))
+        },
+        [changeLocation, bcPath]
     )
 
     const tabs = useStandardViewTabs(depth)

758736c90abc2fb9e37b6ceeacff644323d326b5
cxbox <cxbox.mail@gmail.com>
2024-09-12 13:15:18 +0300

    confirm with title and button names example

--- src/main/java/org/demo/conf/cxbox/extension/action/ActionsExt.java
+++ src/main/java/org/demo/conf/cxbox/extension/action/ActionsExt.java
@@ -2,10 +2,10 @@ package org.demo.conf.cxbox.extension.action;
 
 import java.util.HashMap;
 import java.util.Map;
-import lombok.experimental.UtilityClass;
 import org.cxbox.core.dto.rowmeta.PreAction;
+import org.cxbox.core.dto.rowmeta.PreAction.PreActionBuilder;
+import org.cxbox.core.dto.rowmeta.PreActionType;
 
-@UtilityClass
 public class ActionsExt {
 
 	public static PreAction confirmWithCustomWidget(String message, String widget, String yesButton, String noButton) {
@@ -21,6 +21,27 @@ public class ActionsExt {
 			customParameters.put("noText", noButton);
 		}
 		return PreAction.custom(message, customParameters);
+	}
+
+	public static PreAction confirm(String message, String title, String yesButton, String noButton) {
+		PreActionBuilder preActionBuilder = PreAction.builder()
+				.preActionType(PreActionType.CONFIRMATION);
+		Map<String, String> customParameters = new HashMap<>();
+		if (message != null) {
+			preActionBuilder.message(message);
+		}
+		if (title != null) {
+			customParameters.put("messageContent", title);
+		}
+		if (yesButton != null) {
+			customParameters.put("okText", yesButton);
+		}
+		if (noButton != null) {
+			customParameters.put("cancelText", noButton);
+		}
+		return preActionBuilder
+				.customParameters(customParameters)
+				.build();
 	}
 
 }

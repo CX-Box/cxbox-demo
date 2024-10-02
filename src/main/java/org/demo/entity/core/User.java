9275f1c9ca7d3549fc2b15153c0b2e56de022a83
kotman24 <andreyzaharenko81@gmail.com>
2024-10-02 22:58:37 +0300

    CXBOX-495 | Unnecessary User parameters have been removed

--- src/main/java/org/demo/entity/core/User.java
+++ src/main/java/org/demo/entity/core/User.java
@@ -16,7 +16,6 @@
 
 package org.demo.entity.core;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -24,9 +23,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.time.ZoneId;
 import java.util.List;
-import java.util.Optional;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.StringUtils;

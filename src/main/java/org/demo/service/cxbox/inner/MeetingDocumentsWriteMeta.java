5d9a2d9f1543798e5f98864a35e5150a71ea3c86
cxbox <cxbox.mail@gmail.com>
2024-10-30 16:27:51 +0300

    updated core

--- src/main/java/org/demo/service/cxbox/inner/MeetingDocumentsWriteMeta.java
+++ src/main/java/org/demo/service/cxbox/inner/MeetingDocumentsWriteMeta.java
@@ -1,12 +1,9 @@
 package org.demo.service.cxbox.inner;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 import java.util.Set;
-import org.cxbox.api.data.dto.DataResponseDTO;
-import org.cxbox.api.data.dto.rowmeta.FieldDTO.Hierarchy;
-import org.cxbox.api.data.dto.rowmeta.FieldDTO.Level;
-import org.cxbox.constgen.DtoField;
+import org.cxbox.api.data.dto.hierarhy.grouping.Level;
 import org.cxbox.core.crudma.bc.impl.InnerBcDescription;
 import org.cxbox.core.dto.rowmeta.FieldsMeta;
 import org.cxbox.core.dto.rowmeta.RowDependentFieldsMeta;
@@ -27,11 +24,9 @@ public class MeetingDocumentsWriteMeta extends FieldMetaBuilder<MeetingDocuments
 			Long id, Long parentId) {
 		fields.setEnabled(MeetingDocumentsDTO_.fileId);
 		fields.setEnabled(MeetingDocumentsDTO_.file);
-		fields.setEnabled(
-				MeetingDocumentsDTO_.notes);
+		fields.setEnabled(MeetingDocumentsDTO_.notes);
 		fields.setEnabled(MeetingDocumentsDTO_.briefing);
 		fields.setEnabled(MeetingDocumentsDTO_.document);
-
 		fields.setEnumValues(MeetingDocumentsDTO_.briefing, Briefings.values());
 		fields.setEnumValues(MeetingDocumentsDTO_.document, Documents.values());
 	}
@@ -46,60 +41,46 @@ public class MeetingDocumentsWriteMeta extends FieldMetaBuilder<MeetingDocuments
 		fields.enableSort(MeetingDocumentsDTO_.briefing);
 		fields.enableSort(MeetingDocumentsDTO_.id);
 		fields.enableSort(MeetingDocumentsDTO_.notes);
-
-		groupingHierarchy(
-				fields,
+		fields.groupingHierarchy(
 				MeetingDocumentsDTO_.document,
 				MeetingDocumentsDTO_.briefing,
+				MeetingDocumentsDTO_.document,
 				Set.of(
 						Level.builder(
-								Documents.COMPLIANCE_DOCUMENT,
-								Set.of(
-										Level.builder(
-												Briefings.FINANCIAL_BRIEFING
-										).build(),
-										Level.builder(
-												Briefings.PROJECT_BRIEFING
-										).build()
+										Documents.COMPLIANCE_DOCUMENT,
+										Set.of(
+												Level.builder(
+														Briefings.FINANCIAL_BRIEFING,
+														Set.of(
+																Level.builder(
+																		Documents.REFERENCE_DOCUMENT
+																).build())
+												).build(),
+												Level.builder(
+														Briefings.PROJECT_BRIEFING,
+														Set.of(Level.builder(
+																Documents.TECHNICAL_DOCUMENT
+														).build())
+												).build()
+										)
 								)
-						).build(),
+								.options(Map.of("asd", "asd"))
+								.build(),
 						Level.builder(
-								Documents.POLICY_DOCUMENT,
-								Set.of(
-										Level.builder(
-												Briefings.SECURITY_BRIEFING
-										).build()
+										Documents.POLICY_DOCUMENT,
+										Set.of(
+												Level.builder(
+																Briefings.SECURITY_BRIEFING,
+																Set.of(
+																		Level.builder(
+																						Documents.LEGAL_DOCUMENT)
+																				.build())
+														)
+														.build())
 								)
-						).build()
+								.build()
 				)
 		);
-	}
-
-	private <D extends DataResponseDTO, E1> void groupingHierarchy(FieldsMeta<D> fields,
-			DtoField<D, E1> field1,
-			Set<? extends Level<E1, ?>> levels) {
-
-		Hierarchy hierarchy = new Hierarchy(List.of(field1.getName()), levels);
-		Optional.ofNullable(fields.get(field1.getName())).ifPresent(fieldDTO -> fieldDTO.setGroupingHierarchy(hierarchy));
-	}
-
-	private <D extends DataResponseDTO, E1, E2> void groupingHierarchy(FieldsMeta<D> fields,
-			DtoField<D, E1> field1,
-			DtoField<D, E2> field2,
-			Set<? extends Level<E1, ? extends Level<E2, ?>>> levels) {
-
-		Hierarchy hierarchy = new Hierarchy(List.of(field1.getName(), field2.getName()), levels);
-		Optional.ofNullable(fields.get(field1.getName())).ifPresent(fieldDTO -> fieldDTO.setGroupingHierarchy(hierarchy));
-	}
-
-
-	private <D extends DataResponseDTO, E1, E2, E3> void groupingHierarchy(FieldsMeta<D> fields,
-			DtoField<D, E1> field1,
-			DtoField<D, E2> field2,
-			DtoField<D, E3> field3,
-			Set<? extends Level<E1, ? extends Level<E2, ? extends Level<E3, ?>>>> levels) {
-		Hierarchy hierarchy = new Hierarchy(List.of(field1.getName(), field2.getName(), field3.getName()), levels);
-		Optional.ofNullable(fields.get(field1.getName())).ifPresent(fieldDTO -> fieldDTO.setGroupingHierarchy(hierarchy));
 	}
 
 }

5475e6db8832ab4573dcca7ffccd751dafef66a1
romadim <100356095+romadim@users.noreply.github.com>
2024-12-25 17:36:36 +0300

    CXBOX-686 | Fixed scrolling to the active entry out of visibility when adding new entries (#322)
    * CXBOX-686 | Fixed scrolling to the active entry out of visibility when adding new entries
    
    * CXBOX-686 | Fixed enabling the editing form in table mode for group hierarchy
    
    * CXBOX-686 | changed behavior when bulk uploading files
    
    ---------
    
    Co-authored-by: DROMANE <dmitry.romanenko@weigandt-consulting.com>

--- ui/src/components/widgets/Table/Table.tsx
+++ ui/src/components/widgets/Table/Table.tsx
@@ -182,7 +182,7 @@ function Table<T extends CustomDataItem>({
     const bcData = useAppSelector(state => state.data[meta.bcName] as T[] | undefined)
 
     const expandedRowKeys = useMemo(() => {
-        if (isGroupingHierarchy) {
+        if (enabledGrouping) {
             const expandedRowKey = getGroupingHierarchyRowKeyByRecordId(expandedRowId)
 
             return expandedRowKey && !expandedParentRowKeys.includes(expandedRowKey)
@@ -191,7 +191,7 @@ function Table<T extends CustomDataItem>({
         }
 
         return expandedRowId ? [expandedRowId, ...expandedParentRowKeys] : expandedParentRowKeys
-    }, [expandedParentRowKeys, expandedRowId, getGroupingHierarchyRowKeyByRecordId, isGroupingHierarchy])
+    }, [enabledGrouping, expandedParentRowKeys, expandedRowId, getGroupingHierarchyRowKeyByRecordId])
 
     const needHideActions = useCallback(
         (record: T) => {

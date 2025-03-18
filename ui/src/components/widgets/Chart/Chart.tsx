ed68ad48dd49d63bc183b26316f18ece1d03a041
akozlov <akozlov@phoenixit.ru>
2025-03-18 12:33:29 +0300

    CXBOX-795 | Dashboards. Line2D, DualAxes2D (add limit check)

--- ui/src/components/widgets/Chart/Chart.tsx
+++ ui/src/components/widgets/Chart/Chart.tsx
@@ -1,10 +1,12 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Empty } from 'antd'
+import { useTranslation } from 'react-i18next'
 import Table from '@components/widgets/Table/Table'
 import ChartToggleButton from './components/ChartToggleButton/ChartToggleButton'
 import Pie1D from './components/Pie1D/Pie1D'
 import Chart2D from './components/Chart2D/Chart2D'
 import DualAxes2D from './components/DualAxes2D/DualAxes2D'
+import { useCheckLimit } from '@hooks/useCheckLimit'
 import { useAppSelector } from '@store'
 import { getChartIconByWidgetType } from './utils'
 import { AppWidgetTableMeta, Chart2DWidgetMeta, CustomWidgetTypes, DualAxes2DWidgetMeta, Pie1DWidgetMeta } from '@interfaces/widget'
@@ -14,13 +16,23 @@ interface ChartProps {
 }
 
 const Chart: React.FC<ChartProps> = ({ meta }) => {
+    const { t } = useTranslation()
+
     const [isTableView, setIsTableView] = useState(false)
 
     const data = useAppSelector(state => state.data[meta.bcName])
 
+    const { bcCount, bcPageLimit, isIncorrectLimit } = useCheckLimit(meta.bcName)
+
     const toggleTableView = useCallback(() => {
         setIsTableView(prevState => !prevState)
     }, [])
+
+    useEffect(() => {
+        if (isIncorrectLimit) {
+            setIsTableView(true)
+        }
+    }, [isIncorrectLimit])
 
     if (!data?.length) {
         return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
@@ -42,7 +54,20 @@ const Chart: React.FC<ChartProps> = ({ meta }) => {
 
     return (
         <div>
-            <ChartToggleButton chartIcon={getChartIconByWidgetType(meta.type)} isTableView={isTableView} onClick={toggleTableView} />
+            <ChartToggleButton
+                chartIcon={getChartIconByWidgetType(meta.type)}
+                tooltipTitle={
+                    isIncorrectLimit
+                        ? t(
+                              `Warning! {{count}} rows were fetched from backend - limit for "Chart" mode is {{limit}}. Only "List" mode is available`,
+                              { limit: bcPageLimit, count: bcCount }
+                          )
+                        : undefined
+                }
+                disabled={isIncorrectLimit}
+                isTableView={isTableView}
+                onClick={toggleTableView}
+            />
 
             {isTableView ? <Table meta={meta as unknown as AppWidgetTableMeta} /> : getChartByWidgetType()}
         </div>
--- ui/src/components/widgets/Chart/components/ChartToggleButton/ChartToggleButton.tsx
+++ ui/src/components/widgets/Chart/components/ChartToggleButton/ChartToggleButton.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import { Icon } from 'antd'
+import { Icon, Tooltip } from 'antd'
 import Button from '@components/ui/Button/Button'
 import styles from './ChartToggleButton.module.css'
 
 interface ChartToggleButtonProps {
     chartIcon: string
     isTableView: boolean
+    disabled: boolean
+    tooltipTitle?: string
     onClick: () => void
 }
 
-const ChartToggleButton: React.FC<ChartToggleButtonProps> = ({ chartIcon, isTableView, onClick }) => {
+const ChartToggleButton: React.FC<ChartToggleButtonProps> = ({ chartIcon, isTableView, disabled, tooltipTitle, onClick }) => {
     return (
-        <Button className={styles.container} type="empty" onClick={onClick}>
-            <Icon type={isTableView ? chartIcon : 'table'} />
-        </Button>
+        <Tooltip title={tooltipTitle} trigger="hover" placement="topRight">
+            <div className={styles.container}>
+                <Button className={styles.chartButton} disabled={disabled} type="empty" onClick={onClick}>
+                    <Icon type={isTableView ? chartIcon : 'table'} />
+                </Button>
+            </div>
+        </Tooltip>
     )
 }
 

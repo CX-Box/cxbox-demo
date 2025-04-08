c3590714d72ad2cca3888e518b1cf7892f670cbd
dvasilchuk <nuvolanam@gmail.com>
2025-04-08 18:34:33 +0300

    del

--- ui/src/reducers/screen.ts
+++ ui/src/reducers/screen.ts
@@ -64,9 +64,6 @@ const screenReducerBuilder = reducers
     })
     .addCase(actions.updateBcData, (state, action) => {
         const { bcName } = action.payload
-        if (!bcName) {
-            console.warn('bcName is not set')
-        }
 
         state.bo.bc[bcName] = state.bo.bc[bcName] ?? {}
         state.bo.bc[bcName].loading = false

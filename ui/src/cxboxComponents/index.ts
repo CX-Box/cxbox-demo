93fcefe027d28f2a75d2b206a9ea639b71803cba
DROMANE <dmitry.romanenko@weigandt-consulting.com>
2025-05-28 22:58:12 +0300

    Moved RowOperations

--- ui/src/cxboxComponents/index.ts
+++ ui/src/cxboxComponents/index.ts
@@ -6,8 +6,6 @@ export { default as Dictionary } from '@cxboxComponents/ui/Dictionary/Dictionary
 export { default as MultiValueListRecord } from '@cxboxComponents/MultiValueListRecord/MultiValueListRecord'
 export { default as SearchHighlight } from '@cxboxComponents/ui/SearchHightlight/SearchHightlight'
 export { default as FilterField } from '@cxboxComponents/ui/FilterField/FilterField'
-export { RowOperationsMenu } from './RowOperations/RowOperationsMenu'
-export { default as RowOperationsButton } from './RowOperations/RowOperationsButton'
 
 // Containers
 export { default as View } from './View/View'

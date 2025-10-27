ef83ba03ed65570f1671f60e566facd8514dc61a
Il'ya <33763627+Lourence94@users.noreply.github.com>
2025-10-27 15:52:41 +0300

    CXBOX-1072 | Grouping hierarchy list fallback (#540)
    CXBOX-1072 | Grouping hierarchy list fallback

--- ui/src/constants/pagination.ts
+++ ui/src/constants/pagination.ts
@@ -5,13 +5,23 @@ export const DEFAULT_PAGE_LIMIT = 5
 
 export const AVAILABLE_LIMITS_LIST = [5, 10, 15, 20]
 
-export const AVAILABLE_PAGINATION_TYPES = ['nextAndPreviousWithHasNext', 'nextAndPreviousSmart', 'nextAndPreviousWithCount'] as const
+export const PAGINATION_MODES = {
+    nextAndPreviousWithHasNext: 'nextAndPreviousWithHasNext',
+    nextAndPreviousWithCount: 'nextAndPreviousWithCount',
+    nextAndPreviousSmart: 'nextAndPreviousSmart'
+} as const
 
-export type PaginationMode = (typeof AVAILABLE_PAGINATION_TYPES)[number]
+export const AVAILABLE_PAGINATION_TYPES = [
+    PAGINATION_MODES.nextAndPreviousWithHasNext,
+    PAGINATION_MODES.nextAndPreviousSmart,
+    PAGINATION_MODES.nextAndPreviousWithCount
+] as const
 
-export const MAIN_DEFAULT_PAGINATION_TYPE: PaginationMode = 'nextAndPreviousSmart'
+export type PaginationMode = keyof typeof PAGINATION_MODES
 
-export const SECONDARY_DEFAULT_PAGINATION_TYPE_WITH_COUNT: PaginationMode = 'nextAndPreviousWithCount'
+export const MAIN_DEFAULT_PAGINATION_TYPE = PAGINATION_MODES.nextAndPreviousSmart
+
+export const SECONDARY_DEFAULT_PAGINATION_TYPE_WITH_COUNT = PAGINATION_MODES.nextAndPreviousWithCount
 
 export const WIDGETS_WITH_SECONDARY_DEFAULT_PAGINATION_TYPE_WITH_COUNT: (WidgetTypes | CustomWidgetTypes)[] = [
     WidgetTypes.List,

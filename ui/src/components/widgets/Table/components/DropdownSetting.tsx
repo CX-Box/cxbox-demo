a7f1070d7d19a5b2451057f46e21a58f1e8107ce
ikorneev <imkor34@mail.ru>
2025-11-14 08:01:11 +0300

    CXBOX-985 | Table settings replacement

--- ui/src/components/widgets/Table/components/DropdownSetting.tsx
+++ ui/src/components/widgets/Table/components/DropdownSetting.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { Dropdown, Icon } from 'antd'
 import Button from '../../../ui/Button/Button'
 import { DropDownProps } from 'antd/lib/dropdown'
+import styles from './DropdownSettings.module.less'
 
 interface DropdownSettingProps extends Omit<DropDownProps, 'trigger'> {
     buttonClassName?: string
     buttonIcon?: string
+    error?: boolean
 }
 
-function DropdownSetting({ buttonClassName, buttonIcon, ...restProps }: DropdownSettingProps) {
+function DropdownSetting({ buttonClassName, buttonIcon, error, ...restProps }: DropdownSettingProps) {
     return (
         <Dropdown {...restProps} trigger={['click']}>
             <Button type="empty" className={buttonClassName}>
                 <Icon type={buttonIcon ?? 'setting'} />
+                {error ? <Icon type={'exclamation-circle'} className={styles.errorIcon} /> : null}
             </Button>
         </Dropdown>
     )

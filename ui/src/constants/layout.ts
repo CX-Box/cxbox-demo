47704a70893d520a84f3af491e04bef26d034a46
akozlov <akozlov@phoenixit.ru>
2026-02-19 16:21:48 +0300

    CXBOX-715 | Fixed. Front. GridWidth support for all popups (fix layout)

--- ui/src/constants/layout.ts
+++ ui/src/constants/layout.ts
@@ -1,3 +1,6 @@
+import { WidgetTypes } from '@cxbox-ui/core'
 import { CustomWidgetTypes } from '@interfaces/widget'
 
 export const sidebarWidgetsTypes: string[] = [CustomWidgetTypes.AdditionalInfo, CustomWidgetTypes.AdditionalList]
+
+export const popupWidgets = [WidgetTypes.AssocListPopup, WidgetTypes.PickListPopup, WidgetTypes.FlatTreePopup, CustomWidgetTypes.FormPopup]

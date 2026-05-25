caad695619aa47bbd75eb06f4a4ca61af2cab14f
romadim <100356095+romadim@users.noreply.github.com>
2026-05-25 11:49:22 +0300

    CXBOX-1322 | Added. Translations for RichText (#633)
    Co-authored-by: DROMANE <dmitry.romanenko@weigandt-consulting.com>

--- ui/src/components/RichText/wysiwyg/components/MenuBar.tsx
+++ ui/src/components/RichText/wysiwyg/components/MenuBar.tsx
@@ -25,6 +25,7 @@ import {
     Gear
 } from '@gravity-ui/icons'
 import React, { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 
 interface Props {
     editor: Editor
@@ -33,16 +34,20 @@ interface Props {
 }
 
 export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: Props) {
+    const { t } = useTranslation()
+
     const items: readonly MenuItemType[] = useMemo(
         () => [
             {
                 icon: <ArrowUturnCcwLeft width={16} height={16} />,
-                title: 'Undo',
+                key: 'Undo',
+                title: t('Undo'),
                 action: () => editor.chain().focus().undo().run()
             },
             {
                 icon: <ArrowUturnCwRight width={16} height={16} />,
-                title: 'Redo',
+                key: 'Redo',
+                title: t('Redo'),
                 action: () => editor.chain().focus().redo().run()
             },
             {
@@ -50,25 +55,29 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
             },
             {
                 icon: <Bold width={16} height={16} />,
-                title: 'Bold',
+                key: 'Bold',
+                title: t('Bold'),
                 action: () => editor.chain().focus().toggleBold().run(),
                 isActiveName: 'bold'
             },
             {
                 icon: <Italic width={16} height={16} />,
-                title: 'Italic',
+                key: 'Italic',
+                title: t('Italic'),
                 action: () => editor.chain().focus().toggleItalic().run(),
                 isActiveName: 'italic'
             },
             {
                 icon: <Underline width={16} height={16} />,
-                title: 'Underline',
+                key: 'Underline',
+                title: t('Underline'),
                 action: () => editor.chain().focus().toggleUnderline().run(),
                 isActiveName: 'underline'
             },
             {
                 icon: <Strikethrough width={16} height={16} />,
-                title: 'Strikethrough',
+                key: 'Strikethrough',
+                title: t('Strikethrough'),
                 action: () => editor.chain().focus().toggleStrike().run(),
                 isActiveName: 'strike'
             },
@@ -77,52 +86,60 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
             },
             {
                 icon: <Heading width={16} height={16} />,
-                title: 'Heading',
+                key: 'Heading',
+                title: t('Heading'),
                 isActiveName: 'heading',
                 items: [
                     {
                         icon: <Text width={16} height={16} />,
-                        title: 'Text',
+                        key: 'Text',
+                        title: t('Text'),
                         action: () => editor.chain().focus().setParagraph().run()
                     },
                     {
                         icon: <Heading width={16} height={16} />,
-                        title: 'Heading 1',
+                        key: 'Heading 1',
+                        title: t('Heading 1'),
                         action: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
                         isActiveName: 'heading',
                         isActiveAttrs: { level: 1 }
                     },
                     {
                         icon: <Heading width={16} height={16} />,
-                        title: 'Heading 2',
+                        key: 'Heading 2',
+                        title: t('Heading 2'),
                         action: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
                         isActiveName: 'heading',
                         isActiveAttrs: { level: 2 }
                     },
                     {
                         icon: <Heading width={16} height={16} />,
-                        title: 'Heading 3',
+                        key: 'Heading 3',
+                        title: t('Heading 3'),
                         action: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
                         isActiveName: 'heading',
                         isActiveAttrs: { level: 3 }
                     },
                     {
                         icon: <Heading width={16} height={16} />,
-                        title: 'Heading 4',
+                        key: 'Heading 4',
+                        title: t('Heading 4'),
                         action: () => editor.chain().focus().toggleHeading({ level: 4 }).run(),
                         isActiveName: 'heading',
                         isActiveAttrs: { level: 4 }
                     },
                     {
                         icon: <Heading width={16} height={16} />,
-                        title: 'Heading 5',
+                        key: 'Heading 5',
+                        title: t('Heading 5'),
                         action: () => editor.chain().focus().toggleHeading({ level: 5 }).run(),
                         isActiveName: 'heading',
                         isActiveAttrs: { level: 5 }
                     },
                     {
                         icon: <Heading width={16} height={16} />,
-                        title: 'Heading 6',
+                        key: 'Heading 6',
+                        title: t('Heading 6'),
                         action: () => editor.chain().focus().toggleHeading({ level: 6 }).run(),
                         isActiveName: 'heading',
                         isActiveAttrs: { level: 6 }
@@ -131,46 +148,54 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
             },
             {
                 icon: <ListUl width={16} height={16} />,
-                title: 'List',
+                key: 'List',
+                title: t('List'),
                 items: [
                     {
                         icon: <ListUl width={16} height={16} />,
-                        title: 'Bullet List',
+                        key: 'Bullet List',
+                        title: t('Bullet List'),
                         action: () => editor.chain().focus().toggleBulletList().run(),
                         isActiveName: 'bulletList'
                     },
                     {
                         icon: <ListOl width={16} height={16} />,
-                        title: 'Ordered List',
+                        key: 'Ordered List',
+                        title: t('Ordered List'),
                         action: () => editor.chain().focus().toggleOrderedList().run(),
                         isActiveName: 'orderedList'
                     },
                     {
                         icon: <TextIndent width={16} height={16} />,
-                        title: 'Sink Item',
+                        key: 'Sink Item',
+                        title: t('Sink Item'),
                         action: () => editor.chain().focus().sinkListItem('listItem').run()
                     },
                     {
                         icon: <TextOutdent width={16} height={16} />,
-                        title: 'Lift Item',
+                        key: 'Lift Item',
+                        title: t('Lift Item'),
                         action: () => editor.chain().focus().liftListItem('listItem').run()
                     }
                 ]
             },
             {
                 icon: <Code width={16} height={16} />,
-                title: 'Code',
+                key: 'Code',
+                title: t('Code'),
                 isActiveName: 'code',
                 items: [
                     {
                         icon: <Code width={16} height={16} />,
-                        title: 'Inline code',
+                        key: 'Inline code',
+                        title: t('Inline code'),
                         action: () => editor.chain().focus().toggleCode().run(),
                         isActiveName: 'code'
                     },
                     {
                         icon: <Terminal width={16} height={16} />,
-                        title: 'Code block',
+                        key: 'Code block',
+                        title: t('Code block'),
                         action: () => editor.chain().focus().toggleCodeBlock().run(),
                         isActiveName: 'codeBlock'
                     }
@@ -181,60 +206,69 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
             },
             {
                 icon: <Font width={16} height={16} />,
-                title: 'Text color',
+                key: 'Text color',
+                title: t('Text color'),
                 isActiveName: 'colorify',
-                groupName: 'Text',
+                groupName: t('Text'),
                 items: [
                     {
                         icon: <Font width={16} height={16} color="var(--colorify-default)" />,
-                        title: 'Default',
+                        key: 'Default',
+                        title: t('Default'),
                         action: () => editor.chain().focus().unsetColorify().run()
                     },
                     {
                         icon: <Font width={16} height={16} color="var(--colorify-gray)" />,
-                        title: 'Gray',
+                        key: 'Gray',
+                        title: t('Gray'),
                         action: () => editor.chain().focus().toggleColorify('gray').run(),
                         isActiveName: 'colorify',
                         isActiveAttrs: { color: 'gray' }
                     },
                     {
                         icon: <Font width={16} height={16} color="var(--colorify-yellow)" />,
-                        title: 'Yellow',
+                        key: 'Yellow',
+                        title: t('Yellow'),
                         action: () => editor.chain().focus().toggleColorify('yellow').run(),
                         isActiveName: 'colorify',
                         isActiveAttrs: { color: 'yellow' }
                     },
                     {
                         icon: <Font width={16} height={16} color="var(--colorify-orange)" />,
-                        title: 'Orange',
+                        key: 'Orange',
+                        title: t('Orange'),
                         action: () => editor.chain().focus().toggleColorify('orange').run(),
                         isActiveName: 'colorify',
                         isActiveAttrs: { color: 'orange' }
                     },
                     {
                         icon: <Font width={16} height={16} color="var(--colorify-red)" />,
-                        title: 'Red',
+                        key: 'Red',
+                        title: t('Red'),
                         action: () => editor.chain().focus().toggleColorify('red').run(),
                         isActiveName: 'colorify',
                         isActiveAttrs: { color: 'red' }
                     },
                     {
                         icon: <Font width={16} height={16} color="var(--colorify-green)" />,
-                        title: 'Green',
+                        key: 'Green',
+                        title: t('Green'),
                         action: () => editor.chain().focus().toggleColorify('green').run(),
                         isActiveName: 'colorify',
                         isActiveAttrs: { color: 'green' }
                     },
                     {
                         icon: <Font width={16} height={16} color="var(--colorify-blue)" />,
-                        title: 'Blue',
+                        key: 'Blue',
+                        title: t('Blue'),
                         action: () => editor.chain().focus().toggleColorify('blue').run(),
                         isActiveName: 'colorify',
                         isActiveAttrs: { color: 'blue' }
                     },
                     {
                         icon: <Font width={16} height={16} color="var(--colorify-violet)" />,
-                        title: 'Violet',
+                        key: 'Violet',
+                        title: t('Violet'),
                         action: () => editor.chain().focus().toggleColorify('violet').run(),
                         isActiveName: 'colorify',
                         isActiveAttrs: { color: 'violet' }
@@ -243,7 +277,8 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
             },
             {
                 icon: <QuoteClose width={16} height={16} />,
-                title: 'Quote',
+                key: 'Quote',
+                title: t('Quote'),
                 action: () => editor.chain().focus().toggleBlockquote().run(),
                 isActiveName: 'blockquote'
             },
@@ -253,11 +288,12 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
 
             {
                 icon: <Eraser width={16} height={16} />,
-                title: 'Clear Format',
+                key: 'Clear Format',
+                title: t('Clear Format'),
                 action: () => editor.chain().focus().clearNodes().unsetAllMarks().run()
             }
         ],
-        [editor]
+        [editor, t]
     )
 
     const editorState = useEditorState({
@@ -266,8 +302,8 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
             const activeStates: Record<string, boolean> = {}
 
             const checkItem = (item: MenuItemType) => {
-                if (item.type !== 'divider' && item.isActiveName && item.title) {
-                    activeStates[item.title as string] = ctx.editor?.isActive(item.isActiveName, item.isActiveAttrs) ?? false
+                if (item.type !== 'divider' && item.isActiveName && item.key) {
+                    activeStates[item.key as string] = ctx.editor?.isActive(item.isActiveName, item.isActiveAttrs) ?? false
                 }
 
                 if (item.type !== 'divider' && item.items && Array.isArray(item.items)) {
@@ -304,12 +340,12 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
             icon: item.icon,
             title: item.title,
             action: item.action,
-            isActive: isActive(item.title),
-            disabled: isDisabled(item.title),
+            isActive: isActive(item.key),
+            disabled: isDisabled(item.key),
             items: item.items?.map(subItem => ({
                 ...subItem,
-                isActive: isActive(subItem.title),
-                disabled: isDisabled(subItem.title)
+                isActive: isActive(subItem.key),
+                disabled: isDisabled(subItem.key)
             }))
         }
     })
@@ -319,12 +355,14 @@ export default function MenuBar({ editor, onViewModeChange, toolbarDisabled }: P
             toolbarDisabled={toolbarDisabled}
             items={menuItems}
             rightButton={{
+                key: 'Settings',
                 icon: <Gear width={16} height={16} />,
-                title: 'Settings',
+                title: t('Settings'),
                 items: [
                     {
+                        key: 'Markdown markup',
                         icon: <LogoMarkdown width={16} height={16} />,
-                        title: 'Markdown markup',
+                        title: t('Markdown markup'),
                         action: () => onViewModeChange('source')
                     }
                 ]

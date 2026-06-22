e8ede6662f52d1f96af0b62957052fc4de1bb8ac
agelfman <agelfman@cxlab.ru>
2026-06-22 15:56:15 +0300

    CXBOX-1321| Fixed. Fixed dashboard

--- src/test/java/org/demo/test/oracle/OracleTest.java
+++ src/test/java/org/demo/test/oracle/OracleTest.java
@@ -1,5 +1,7 @@
 package org.demo.test.oracle;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -29,7 +31,6 @@ class OracleTest {
 
 	@Test
 	void contextStarts() {
-		//startContext
+		assertNotNull(oracle);
 	}
-
 }

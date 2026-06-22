ca6e4615a9831fff76ade277031abc7e9ed6d942
agelfman <agelfman@cxlab.ru>
2026-06-22 13:17:27 +0300

    CXBOX-1321| Fixed. Fixed dashboard

--- src/main/java/org/demo/conf/uploadfile/PdfUploadMeetingDocuments.java
+++ src/main/java/org/demo/conf/uploadfile/PdfUploadMeetingDocuments.java
@@ -10,7 +10,6 @@ import org.demo.entity.MeetingDocuments;
 import org.demo.entity.enums.DocumentStatus;
 import org.demo.repository.MeetingDocumentsRepository;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
@@ -20,26 +19,23 @@ import org.springframework.stereotype.Component;
 public class PdfUploadMeetingDocuments {
 
 	private final FileService fileService;
-
 	private final MeetingDocumentsRepository meetingDocumentsRepository;
-
-	@Profile("!oracle-test")
 	@EventListener(ApplicationReadyEvent.class)
-	public void uploadPdf() {
+	public void uploadPdf()  {
 		try {
-			Optional<MeetingDocuments> meetingDocuments = meetingDocumentsRepository.findById(1L);
-			if (meetingDocuments.isPresent()) {
+			Optional<MeetingDocuments> meetingDocuments =  meetingDocumentsRepository.findById(1L);
+			if(meetingDocuments.isPresent()) {
 				meetingDocuments.get().setFile("meeting.pdf");
-				meetingDocuments.get().setFileId(getFileId("meeting.pdf", "application/pdf"));
+				meetingDocuments.get().setFileId(getFileId("meeting.pdf","application/pdf"));
 
 				meetingDocuments.get().setFileEncrypt("MyEncrypt.enc");
-				meetingDocuments.get().setFileEncryptId(getFileId("MyEncrypt.enc", "application/pdf"));
+				meetingDocuments.get().setFileEncryptId(getFileId("MyEncrypt.enc","application/pdf"));
 
 				meetingDocuments.get().setFileSign("MySign.sig");
-				meetingDocuments.get().setFileSignId(getFileId("MySign.sig", "application/pdf"));
+				meetingDocuments.get().setFileSignId(getFileId("MySign.sig","application/pdf"));
 
 				meetingDocuments.get().setFileEncryptAndSign("meeting.zip");
-				meetingDocuments.get().setFileEncryptAndSignId(getFileId("meeting.zip", "application/zip"));
+				meetingDocuments.get().setFileEncryptAndSignId(getFileId("meeting.zip" ,"application/zip"));
 
 				meetingDocuments.get().setStatus(DocumentStatus.SIGNED);
 
@@ -51,21 +47,21 @@ public class PdfUploadMeetingDocuments {
 		}
 	}
 
-	private String getFileId(String objectName, String type) throws IOException {
-		ClassPathResource resource = new ClassPathResource("signdoc/" + objectName);
+	private String getFileId(String objectName,String type) throws IOException {
+		ClassPathResource resource = new ClassPathResource("signdoc/"+objectName);
 
 		FileDownloadDto file = new FileDownloadDto(
-				() -> {
-					try {
-						return new FileInputStream(resource.getFile());
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-				},
-				resource.contentLength(),
+			 () -> {
+				 try {
+					 return new FileInputStream(resource.getFile());
+				 } catch (IOException e) {
+					 throw new RuntimeException(e);
+				 }
+			 },
+			 resource.contentLength(),
 				objectName,
 				type
-		);
+	 );
 
 		return fileService.upload(file, objectName);
 	}

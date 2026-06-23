df17466fe21bc0392faa69a270fe34af539e673d
agelfman <agelfman@cxlab.ru>
2026-06-23 20:59:45 +0300

    CXBOX-1309| Fixed. Fixed dashboard

--- src/main/java/org/demo/dto/cxbox/inner/SaleSellerStatsDTO.java
+++ src/main/java/org/demo/dto/cxbox/inner/SaleSellerStatsDTO.java
@@ -1,20 +1,27 @@
 package org.demo.dto.cxbox.inner;
 
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.cxbox.api.data.dto.DataResponseDTO;
+import org.demo.entity.enums.SaleStatus;
 
 @Getter
 @Setter
 @Accessors(chain = true)
 public class SaleSellerStatsDTO extends DataResponseDTO {
 
+	private	String clientName;
 	private String sellerName;
-
 	private Long sum;
-
-	private String clientName;
-
+	private	Long saleCount;
+	private	SaleStatus status;
+	private	LocalDateTime firstSaleDate;
+	private LocalDateTime lastActivityDate;
+	private Double avgB2bDeal;
+	private Long maxContract;
+	private Long confirmedRevenue;
+	private Long openPipeline;
 
 }

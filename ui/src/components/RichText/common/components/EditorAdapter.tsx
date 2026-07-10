96e5ac87aec5263cc6aa7165faee40ba3c35da28
DROMANE <dmitry.romanenko@weigandt-consulting.com>
2026-07-10 17:10:26 +0300

    FIX

--- ui/src/components/RichText/common/components/EditorAdapter.tsx
+++ ui/src/components/RichText/common/components/EditorAdapter.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { ViewMode } from '@components/RichText/common/types'
 import WysiwygEditor from '@components/RichText/wysiwyg/components/Editor'
 import SourceEditor from '@components/RichText/source/components/Editor'
-import { TEXTAREA_VERTICAL_PADDING_OFFSET } from '@components/RichText/constants'
+import { ONE_ROW_TEXTAREA_VERTICAL_PADDING_OFFSET, COMMON_TEXTAREA_VERTICAL_PADDING_OFFSET } from '@components/RichText/constants'
 import { useBoundedResizableHeight } from '@components/RichText/wysiwyg/hooks'
 import { RichTextEditorProps } from '@components/RichText/RichTextEditor'
 import TextClampWrapper from '@components/TextClampWrapper/TextClampWrapper'
@@ -18,15 +18,14 @@ const EditorAdapter: React.FC<RichTextEditorProps> = ({
     minRows,
     maxRows,
     editMinRows,
-    editMaxRows,
-    meta
+    editMaxRows
 }) => {
     const [viewMode, setViewMode] = useState<ViewMode>('wysiwyg')
-
+    const onlyOneRow = editMinRows === editMaxRows && editMinRows === 1
     const { ref: editorWrapperRef, style: wrapperStyle } = useBoundedResizableHeight({
         minRows: editMinRows,
         maxRows: editMaxRows,
-        heightOffset: TEXTAREA_VERTICAL_PADDING_OFFSET,
+        heightOffset: onlyOneRow ? ONE_ROW_TEXTAREA_VERTICAL_PADDING_OFFSET : COMMON_TEXTAREA_VERTICAL_PADDING_OFFSET,
         readOnly
     })
     // Force to wysiwyg in disabled mode
@@ -51,8 +50,6 @@ const EditorAdapter: React.FC<RichTextEditorProps> = ({
             </TextClampWrapper>
         )
     }
-
-    const onlyOneRow = editMinRows === editMaxRows && editMinRows === 1
 
     if (viewMode === 'source') {
         return (

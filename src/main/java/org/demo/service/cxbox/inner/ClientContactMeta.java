8104946d27d56ce84b585ffa53382b67ba2b0ea5
cxbox <cxbox.mail@gmail.com>
2026-09-14 03:25:42 +0300

    CXBOX-1389 | Fixed. Client contacts: drilldown to contact edit on client edit step, read-only contact lists and Add Existing popup, drilldowns in client sales

--- src/main/java/org/demo/service/cxbox/inner/ClientContactMeta.java
+++ src/main/java/org/demo/service/cxbox/inner/ClientContactMeta.java
@@ -1,9 +1,11 @@
 package org.demo.service.cxbox.inner;
 
 import org.cxbox.core.crudma.bc.impl.InnerBcDescription;
+import org.cxbox.core.dto.DrillDownType;
 import org.cxbox.core.dto.rowmeta.FieldsMeta;
 import org.cxbox.core.dto.rowmeta.RowDependentFieldsMeta;
 import org.cxbox.core.service.rowmeta.FieldMetaBuilder;
+import org.demo.controller.CxboxRestController;
 import org.demo.dto.cxbox.inner.ContactDTO;
 import org.demo.dto.cxbox.inner.ContactDTO_;
 import org.springframework.stereotype.Service;
@@ -15,16 +17,27 @@ public class ClientContactMeta extends FieldMetaBuilder<ContactDTO>  {
 	@Override
 	public void buildRowDependentMeta(RowDependentFieldsMeta<ContactDTO> fields, InnerBcDescription bcDescription,
 			Long id, Long parentId) {
-		fields.setEnabled(
-				ContactDTO_.fullName,
-				ContactDTO_.email,
-				ContactDTO_.phoneNumber
-		);
-		fields.setRequired(
-				ContactDTO_.fullName,
-				ContactDTO_.email,
-				ContactDTO_.phoneNumber
-		);
+		// "Add Existing" popup only selects contacts, its rows are read-only
+		if (!CxboxRestController.contactEditAssoc.isBc(bcDescription)) {
+			fields.setEnabled(
+					ContactDTO_.fullName,
+					ContactDTO_.email,
+					ContactDTO_.phoneNumber
+			);
+			fields.setRequired(
+					ContactDTO_.fullName,
+					ContactDTO_.email,
+					ContactDTO_.phoneNumber
+			);
+			// shown as a link only where the widget declares drillDown (client edit contacts step)
+			fields.setDrilldown(
+					ContactDTO_.fullName,
+					DrillDownType.INNER,
+					"/screen/client/view/clienteditcreatecontact/"
+							+ CxboxRestController.clientEdit + "/" + parentId + "/"
+							+ CxboxRestController.contactEdit + "/" + id
+			);
+		}
 	}
 
 	@Override
